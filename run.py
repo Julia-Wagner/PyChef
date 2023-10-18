9a060e2b80d3f224e3fbe139d15e83284c4d7132
julia Wager <julia@p-wagner.at>
2023-10-18 21:06:42 +0200

    feat: add rich package for text styling

--- run.py
+++ run.py
@@ -2,7 +2,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from pyfiglet import Figlet
-from colorama import Fore
+from rich.console import Console
+from rich.theme import Theme
 
 # -------------------------- API CONNECTION ---------------------------
 SCOPE = [
@@ -18,8 +19,18 @@ SHEET = GSPREAD_CLIENT.open("PyChef")
 
 users = SHEET.worksheet("users")
 
+# ------------------------- STYLING THEMES ---------------------------
+custom_theme = Theme({
+    "error": "bold red",
+    "heading": "bold underline",
+    "option": "yellow"
+})
+
+console = Console(theme=custom_theme)
+
 def show_welcome_message():
-    f = Figlet(font='big')
-    print(Fore.BLUE + f.renderText('PyChef'))
+    f = Figlet(font="big")
+    console.print(f.renderText("PyChef"), style="blue")
+    console.print("Select an option", style="heading")
 
 show_welcome_message()
