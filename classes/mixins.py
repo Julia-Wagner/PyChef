808d466757e9cedae9b464a135303ba2c1176684
julia Wager <julia@p-wagner.at>
2023-10-20 20:47:21 +0200

    feat: add style to mixins

--- classes/mixins.py
+++ classes/mixins.py
@@ -1,4 +1,6 @@
 import os
+from rich.console import Console
+from rich.theme import Theme
 
 
 class ClearConsole:
@@ -10,3 +12,17 @@ class ClearConsole:
         :return:
         """
         return os.system("cls" if os.name in ("nt", "dos") else "clear")
+
+
+class StyleConsole:
+    @staticmethod
+    def style():
+        custom_theme = Theme({
+            "error": "bold red",
+            "heading": "bold underline",
+            "option": "bold",
+            "info": "dim",
+            "success": "bold green"
+        })
+
+        return Console(theme=custom_theme)
