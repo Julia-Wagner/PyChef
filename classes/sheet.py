b32a4ad8848e497a3c5a132413c6ddbfc7f6401e
julia Wager <julia@p-wagner.at>
2023-10-22 11:32:02 +0200

    feat: add docstrings and method to get a worksheet entry to SheetService

--- classes/sheet.py
+++ classes/sheet.py
@@ -20,11 +20,21 @@ class SheetService:
 
     @classmethod
     def get_worksheet(cls, sheet_name):
+        """
+        Get the needed worksheet
+        :param string sheet_name: the name of the worksheet
+        :return: worksheet to work with
+        """
         worksheet = cls.SHEET.worksheet(sheet_name)
         return worksheet
 
     @classmethod
     def increment_id(cls, sheet_name):
+        """
+        Increment the id for the given worksheet
+        :param string sheet_name: the name of the worksheet
+        :return: incremented id
+        """
         last_id = cls.get_worksheet(sheet_name).get_all_values()[-1][0]
 
         if last_id != "id":
@@ -32,3 +42,17 @@ class SheetService:
         else:
             new_id = 1
         return new_id
+
+    @classmethod
+    def get_entry(cls, sheet_name, value, column):
+        """
+        Check if the given value exists in the given column of a specific worksheet
+        :param string sheet_name: the name of the worksheet
+        :param string value: the value to search for in the worksheet
+        :param int column: the column to look in
+        :return: False or found entry
+        """
+        entry = cls.get_worksheet(sheet_name).find(value, in_column=column)
+        if entry is None:
+            return False
+        return entry
